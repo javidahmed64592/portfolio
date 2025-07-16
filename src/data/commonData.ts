687d17a7a00aca300e90a7b70f340dd805061be1
Javid Ahmed <javidahmed@icloud.com>
2025-07-16 23:32:48 +0100

    Add dataDir and dataPath function to commonData for improved asset management

--- src/data/commonData.ts
+++ src/data/commonData.ts
@@ -1,6 +1,8 @@
 const assetsDir = "/assets";
+const dataDir = `${assetsDir}/data`;
 const iconDir = `${assetsDir}/icons`;
 const imageDir = `${assetsDir}/images`;
 
+export const dataPath = (data: string) => `${dataDir}/${data}`;
 export const iconPath = (icon: string) => `${iconDir}/${icon}`;
 export const imagePath = (image: string) => `${imageDir}/${image}`;

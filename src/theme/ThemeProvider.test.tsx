dacd254f484f68d34746075857a3eaf81321e3c0
Javid Ahmed <javidahmed@icloud.com>
2025-07-18 00:42:24 +0100

    Remove unnecessary React import from ThemeProvider test file

--- src/theme/ThemeProvider.test.tsx
+++ src/theme/ThemeProvider.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import React from "react";
 import { defaultTheme, Theme } from "./theme";
 import { ThemeProvider } from "./ThemeProvider";
 import { useTheme } from "./";
